c9c1c7b9443c0ae82ec7da7502dd39cd69198bb8
Administrator <administrator@admin.lan>
2020-09-01 19:51:05 -0500

    Added code for second build

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React V2 for Evan
         </a>
       </header>
     </div>
